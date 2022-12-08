c84c68847384e45119a67f5a153c8b8189962e60
Peyton Moss <pmoss94@gmail.com>
2022-12-08 15:30:14 -0700

    seed and sequelize added

---
+++ server/seed/seed.js
@@ -0,0 +1,111 @@
+let bases = [
+  {
+    name: "Coke",
+    diet: false,
+  },
+  {
+    name: "Diet Coke",
+    diet: true,
+  },
+  {
+    name: "Mtn. Dew",
+    diet: false,
+  },
+  {
+    name: "Diet Mtn. Dew",
+    diet: true,
+  },
+  {
+    name: "Dr. Pepper",
+    diet: false,
+  },
+  {
+    name: "Diet Dr. Pepper",
+    diet: true,
+  },
+  {
+    name: "Sprite",
+    diet: false,
+  },
+  {
+    name: "Sprite Zero",
+    diet: true,
+  },
+  {
+    name: "Barqs Rootbeer",
+    diet: false,
+  },
+  {
+    name: "Diet Barqs Rootbeer",
+    diet: true,
+  },
+];
+
+let extras = [
+  {
+    name: "Vanilla Shot",
+  },
+  {
+    name: "Cherry Shot",
+  },
+  {
+    name: "Coconut Shot",
+  },
+  {
+    name: "Raspberry Shot",
+  },
+
+  {
+    name: "Strawberry Shot",
+  },
+  {
+    name: "Peach Shot",
+  },
+  {
+    name: "Blueberry Shot",
+  },
+  {
+    name: "Passionfruit Shot",
+  },
+  {
+    name: "Watermelon Shot",
+  },
+  {
+    name: "Pomegranet Shot",
+  },
+  {
+    name: "Pinapple Shot",
+  },
+  {
+    name: "Lemon Wedge",
+  },
+  {
+    name: "lime Wedge",
+  },
+  {
+    name: "Coconut Cream",
+  },
+];
+
+let favorites = [
+  {
+    name: "Ractive Rootbeer",
+    baseId: 9,
+    extras: "[1, 14]",
+  },
+  {
+    name: "Console Log Cola",
+    baseId: 1,
+    extras: "[2, 13]",
+  },
+  {
+    name: "Dr. Postman",
+    baseId: 6,
+    extras: "[7, 14]",
+  },
+  {
+    name: "getElementByI-Dew",
+    baseId: 3,
+    extras: "[8, 11, 12]",
+  },
+];
